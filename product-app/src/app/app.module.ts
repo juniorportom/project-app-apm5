0283c5af98488a0b56c9ed71d0f0dce32289a848
Reinaldo Portocarrero <juniorportom@gmail.com>
2016-12-03 12:44:07 -0500

    Registro Usuarios
    Registro de usuarios

--- product-app/src/app/app.module.ts
+++ product-app/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import {UserService} from "../providers/user-service";
+import {RegisterUser} from '../pages/register-user/register-user';
+import {OptionsPage} from '../pages/options/options';
 
 @NgModule({
   declarations: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    RegisterUser,
+    OptionsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -17,8 +22,10 @@ import { Page2 } from '../pages/page2/page2';
   entryComponents: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    RegisterUser,
+    OptionsPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, UserService]
 })
 export class AppModule {}
